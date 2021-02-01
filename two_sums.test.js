8735048e4702de1a7856f1a403a54d622c5e5036
wypratama <wicaksanapratama@gmail.com>
2021-02-02 02:23:26 +0700

    Add unit testing

--- two_sums.test.js
+++ two_sums.test.js
@@ -2,15 +2,15 @@ const twoSum = require('./index');
 
 describe('MC Payment Two Sums Test', () => {
   describe('running basic test', () => {
-    test('should return [ 0, 2 ] when given array [2, 7, 11, 15]', () => {
+    test('should return [ 0, 2 ] when given array [2, 7, 11, 15] and target 13', () => {
       expect(twoSum([2, 7, 11, 15], 13)).toEqual(
         expect.arrayContaining([0, 2])
       );
     });
-    test('should return [ 1, 2 ] when given array [3, 2, 4]', () => {
+    test('should return [ 1, 2 ] when given array [3, 2, 4] and target 6', () => {
       expect(twoSum([3, 2, 4], 6)).toEqual(expect.arrayContaining([1, 2]));
     });
-    test('should return [ 0, 1 ] when given array [3, 3]', () => {
+    test('should return [ 0, 1 ] when given array [3, 3] and target 6', () => {
       expect(twoSum([3, 3], 6)).toEqual(expect.arrayContaining([0, 1]));
     });
   });
